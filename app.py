4930914d3455818afc8c65f44e829c29f240c9c1
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-13 22:28:19 -0400

    added hexadecimal to binary

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import streamlit as st
-from pages import page_one, page_two
+from pages import page_one, page_two, page_three
 
 # Sidebar navigation
 st.sidebar.title("Navigation")
-page = st.sidebar.selectbox("Go to", ["Binary to Decimal", "Decimal to Binary"])
+page = st.sidebar.selectbox("Go to", ["Binary to Decimal", "Decimal to Binary", "Hexadecimal to Binary"])
 
 # Render the selected page
 if page == "Binary to Decimal":
     page_one()
 elif page == "Decimal to Binary":
     page_two()
+elif page == "Hexadecimal to Binary":
+    page_three()
--- pages.py
+++ pages.py
@@ -28,7 +28,7 @@ def page_one():
 def page_two():
     st.title('Decimal to Binary Converter')
 
-    def convertDecimalToBinary(decimal: int):
+    def convertDecimalToBinary(decimal):
         decimal = int(decimal)
         output = ""
         while decimal > 0:
@@ -44,3 +44,43 @@ def page_two():
         # When the button is clicked, display the input
         st.text("Base-2 (binary) number:")
         st.write(convertDecimalToBinary(decimal=decimal)) 
+
+def page_three():
+    st.title('Hexadecimal to Binary Converter')
+    st.write('Hexadecimal makes it easier to understand Binary code as it can get pretty long! Here is a quick program to help you :)')
+    def convertHexadecimaltoBinary(hex):
+        ref = {
+            "0": "0000",
+            "1": "0001",
+            "2": "0010",
+            "3": "0011",
+            "4": "0100",
+            "5": "0101",
+            "6": "0110",
+            "7": "0111",
+            "8": "1000",
+            "9": "1001",
+            "A": "1010",
+            "B": "1011",
+            "C": "1100",
+            "D": "1101",
+            "E": "1110",
+            "F": "1111"
+        }
+        binary_output = ""
+        hex = hex
+        for i in hex:
+            if i.upper() in ref:
+                binary_output = binary_output + ref[i.upper()]
+            else:
+                return "Invalid Base-16 integer (can only include 0-9, A-F)"
+        return binary_output
+    
+    # Input component: Text Input
+    hex = st.text_input("Enter Hex (Base-16) integer:")
+
+    # Button component
+    if st.button('Submit'):
+        # When the button is clicked, display the input
+        st.text("Base-10 (decimal) number:")
+        st.write(convertHexadecimaltoBinary(hex=hex))
